aad7a1698f12604bb4c253e87584885dc6e2d103
Gunny Waffle <davidafoxworthy@gmail.com>
2017-04-17 23:40:04 -0400

    player bullets added with animations

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed;
+
+    void Start ()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+    }
+	
+	void Update ()
+    {
+        Vector3 screenPos = Globals.ClampToScreen(transform.position);
+
+        if ((transform.position - screenPos).magnitude > transform.localScale.magnitude)
+            Destroy(gameObject);
+	}
+}
