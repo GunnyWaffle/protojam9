01dd5173b436a898bbd3d752bfc07c1a13ed0f9d
Austin Willoughby <arw8020@rit.edu>
2017-05-07 15:05:26 -0400

    Credit Screen Update, Intro Animation Comments
    Updated credit scene to look better, added new music, commented intro
    animation script.

--- Assets/Scripts/MenuScripts/IntroAnimation.cs
+++ Assets/Scripts/MenuScripts/IntroAnimation.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class IntroAnimation : MonoBehaviour
 {
     //Public
-    public Camera mainCam;
-    public SpriteRenderer titleBlock;
-    public GameObject bossShip;
-    public MenuHandler menuControl;
+    public Camera mainCam; //The main scene camera
+    public SpriteRenderer titleBlock; //The graphic which blocks the menu visuals
+    public GameObject bossShip; //The boss sprite
+    public MenuHandler menuControl; //The menu handling script
 
     //Private
-    private Color blockColor;
-    private List<Vector4> cameraPosAndLerpTimes;
-    private float timer;
-    private int currentStep;
-    private float flashTimer;
+    private Color blockColor; //Used to set the color of the titleBlock
+    private List<Vector4> cameraPosAndLerpTimes; //A list of camera positions, and the time to lerp between them (Outlined in start)
+    private float timer; //A timer that runs the overall animation
+    private int currentStep; //An index to track where we are in the animation
+    private float flashTimer; //A timer that handles the red "alert" flashing in the animation
+    private bool transitionToMenu; //A bool that tells us if the user has decided to skip the intro
+    private bool doingIntro; //Are we currently animating the intro?
+    private Vector3 cameraStartPos; //The starting position of the camera (used when making impromptu transition to menu)
 
     // Use this for initialization
     void Start()
@@ -23,34 +26,62 @@ public class IntroAnimation : MonoBehaviour
         currentStep = 0;
         timer = 0;
         blockColor = Color.black;
+        transitionToMenu = false;
+        doingIntro = true;
 
         cameraPosAndLerpTimes = new List<Vector4>();
-        cameraPosAndLerpTimes.Add(new Vector4(5.25f, -2.2f, -10.0f, 2.0f));
+        //Define the movement of the camera     X       Y      Z     TIME
+        //Camera moves from point[0].xyx to point[1].xyz in point[1].w seconds
+        cameraPosAndLerpTimes.Add(new Vector4(5.25f , -2.2f, -10.0f, 2.0f));
         cameraPosAndLerpTimes.Add(new Vector4(-6.25f, 2.45f, -10.0f, 7.0f));
-        cameraPosAndLerpTimes.Add(new Vector4(-6.25f, 2.45f, -10.0f, .2f));
-        cameraPosAndLerpTimes.Add(new Vector4(5.25f, -2.2f, -10.0f, 1.0f));
-        cameraPosAndLerpTimes.Add(new Vector4(5.25f, -2.2f, -10.0f, 2.0f));
-        cameraPosAndLerpTimes.Add(new Vector4(0, 0, -10.0f, 2.0f));
+        cameraPosAndLerpTimes.Add(new Vector4(-6.25f, 2.45f, -10.0f, 0.2f));
+        cameraPosAndLerpTimes.Add(new Vector4(5.25f , -2.2f, -10.0f, 1.0f));
+        cameraPosAndLerpTimes.Add(new Vector4(5.25f , -2.2f, -10.0f, 2.0f));
+        cameraPosAndLerpTimes.Add(new Vector4(0     , 0    , -10.0f, 2.0f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        DoCameraLerp(currentStep, currentStep + 1);
-
-        switch (currentStep)
+        //if we are currently running the intro animation
+        if (doingIntro)
         {
-            case 0: StageZero(); break;
-            case 1: StageOne(); break;
-            case 2: StageTwo(); break;
-            case 3: StageThree(); break;
-            case 4: StageFour(); break;
-            case 5: StageFive(); break;
-            default: break;
+            //Increment timer, move the camera
+            timer += Time.deltaTime;
+            DoCameraLerp(currentStep, currentStep + 1);
+
+            //Handle unique events for each stage
+            switch (currentStep)
+            {
+                case 0: StageZero(); break;
+                case 1: StageOne(); break;
+                case 2: StageTwo(); break;
+                case 3: StageThree(); break;
+                case 4: StageFour(); break;
+                case 5: StageFive(); break;
+                default: break;
+            }
+
+            //If the user presses start (to skip the intro)
+            if (Input.GetButtonDown("Start"))
+            {
+                //Transition to the menu, move out of the current step loop, save the cameras position, reset timer
+                transitionToMenu = true;
+                currentStep = 10;
+                doingIntro = false;
+                cameraStartPos = mainCam.transform.position;
+                timer = 0;
+            }
+        }
+        
+        //If the user has chosen to transition straight to the menu
+        if (transitionToMenu)
+        {
+            TransitionToMain();
         }
     }
 
+    //Fades in the scene
     private void StageZero()
     {
         blockColor.r = timer * .5f;
@@ -59,12 +90,14 @@ public class IntroAnimation : MonoBehaviour
         titleBlock.color = blockColor;
     }
 
+    //Move the boss ship towards earth
     private void StageOne()
     {
         bossShip.transform.position = Vec3SmoothLerp(new Vector3(-11.58f, 5.31f, -7.33f), 
             new Vector3(1.75f, -1.3f, -7.33f), timer, cameraPosAndLerpTimes[2].w + cameraPosAndLerpTimes[3].w);
     }
 
+    //Make the boss ship get smaller and move towards earth. Flash the scene red (an alarm in the ship)
     private void StageTwo()
     {
         flashTimer += Time.deltaTime * 5;
@@ -84,6 +117,7 @@ public class IntroAnimation : MonoBehaviour
         bossShip.GetComponent<SpriteRenderer>().color = blockColor;
     }
 
+    //Continue the red flashing. Pause for a second
     private void StageThree()
     {
         flashTimer += Time.deltaTime * 5;
@@ -92,6 +126,7 @@ public class IntroAnimation : MonoBehaviour
         titleBlock.color = blockColor;
     }
 
+    //Continue flashing, scale up the camera as we move to center screen
     private void StageFour()
     {
         flashTimer += Time.deltaTime * 5;
@@ -101,6 +136,7 @@ public class IntroAnimation : MonoBehaviour
         mainCam.GetComponent<Camera>().orthographicSize = Mathf.Lerp(2, 5, Smootherstep(timer *.5f));
     }
 
+    //Safely finish flashing the camera, fade out the menu blocker, give the user ability to select menu options
     private void StageFive()
     {
         if(!(blockColor.g - .99f > 0))
@@ -114,6 +150,36 @@ public class IntroAnimation : MonoBehaviour
         menuControl.SetPlayerControlAbility(true);
     }
 
+    //For when the user wants to skip the intro. Safely transition to the main menu and give the user control.
+    private void TransitionToMain()
+    {
+        timer += Time.deltaTime;
+     
+        bossShip.GetComponent<SpriteRenderer>().color = blockColor;
+        
+        mainCam.transform.position = Vec3SmoothLerp(cameraStartPos, new Vector3(0, 0, -10.0f), timer, 1);
+
+        if (timer >= 0.2f)
+        {
+            mainCam.GetComponent<Camera>().orthographicSize = Mathf.Lerp(mainCam.GetComponent<Camera>().orthographicSize, 5, Smootherstep((timer - .2f) * .65f));
+            if (timer >= 1.0f)
+            {
+                blockColor.r += Time.deltaTime * 2.0f;
+                blockColor.g += Time.deltaTime * 2.0f;
+                blockColor.b += Time.deltaTime * 2.0f;
+                blockColor.a -= Time.deltaTime * 2.0f;
+                titleBlock.color = blockColor;
+
+                if (timer >= 2.0f)
+                {
+                    transitionToMenu = false;
+                    menuControl.SetPlayerControlAbility(true);
+                }
+            }
+        }
+    }
+
+    //Lerp the camera from defined position 1 to position 2
     private void DoCameraLerp(int startIndex, int endIndex)
     {
         if (startIndex >= 0 && startIndex < cameraPosAndLerpTimes.Count && endIndex >= 0 && endIndex < cameraPosAndLerpTimes.Count)
@@ -131,7 +197,7 @@ public class IntroAnimation : MonoBehaviour
         }
     }
 
-    
+    //Lerp from a point to another point (with SmootherStep)
     private Vector3 Vec3SmoothLerp(Vector3 start, Vector3 end, float time, float totalTime)
     {
         Vector3 lerped = new Vector3(
@@ -144,6 +210,7 @@ public class IntroAnimation : MonoBehaviour
 
 
     //From: https://chicounity3d.wordpress.com/2014/05/23/how-to-lerp-like-a-pro/
+    //Smoothly lerp between two points with ease in and ease out
     private float Smootherstep(float t)
     {
         return t * t * t * (t * (6f * t - 15f) + 10f);
